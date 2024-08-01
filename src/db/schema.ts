ad08a19568b34d8a81a1da790f6647bda9773c05
cioff-ultra-dev <cioff.ultra@gmail.com>
2024-08-01 20:46:57 +0000

    events schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -9,3 +9,11 @@ export const usersTable = pgTable("users", {
 
 export type InsertUser = typeof usersTable.$inferInsert;
 export type SelectUser = typeof usersTable.$inferSelect;
+
+export const eventsTable = pgTable("events", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+});
+
+export type InsertEvents = typeof eventsTable.$inferInsert;
+export type SelectEvents = typeof eventsTable.$inferSelect;
