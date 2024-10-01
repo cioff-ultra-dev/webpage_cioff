113ff4aba3f76a88879e4c7d44d48c8176df2397
Carlos Lopez <celb25@gmail.com>
2024-10-01 17:15:44 -0500

    🐛 FIX:  Restore the migrations and meta for DB

--- migrations/0009_typical_changeling.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "prod"."festivals" DROP CONSTRAINT "festivals_email_unique";--> statement-breakpoint
-ALTER TABLE "prod"."festivals" ALTER COLUMN "email" DROP NOT NULL;
--- migrations/0011_large_lockheed.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "prod"."national_section_lang" ADD COLUMN "lang" integer;--> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "prod"."national_section_lang" ADD CONSTRAINT "national_section_lang_lang_languages_id_fk" FOREIGN KEY ("lang") REFERENCES "prod"."languages"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
