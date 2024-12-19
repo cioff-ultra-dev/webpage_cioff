60b6b85954d9fb6b949d478cf16755a3a24d14eb
Carlos Lopez <celb25@gmail.com>
2024-12-19 08:38:29 -0500

    FIX: Temporary comments for stripe implementation.

--- src/app/api/payment/customer/route.ts
+++ src/app/api/payment/customer/route.ts
@@ -3,43 +3,43 @@ import { db } from "@/db";
 import { roles, SelectUser, users } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
-const stripe = new Stripe(process.env.STRIPE_API_KEY!);
+// const stripe = new Stripe(process.env.STRIPE_API_KEY!);
 
 export async function POST(request: Request) {
-  const { email = "" } = await request.json();
-
-  if (!email) {
-    return Response.json(null, { status: 404 });
-  }
-
-  const sq = db
-    .select({ id: roles.id })
-    .from(roles)
-    .where(eq(roles.name, "National Sections"))
-    .limit(1);
-
-  let currentUser: SelectUser | undefined = await db.query.users.findFirst({
-    where(fields, { eq, and }) {
-      return and(eq(fields.email, email), eq(fields.roleId, sq));
-    },
-  });
-
-  if (currentUser && !currentUser.stripeCustomerId) {
-    const customer = await stripe.customers.create({
-      name: `${currentUser.firstname}${
-        currentUser.lastname ? ` ${currentUser.lastname}` : ""
-      }`,
-      email: currentUser.email,
-    });
-
-    if (customer.id) {
-      [currentUser] = await db
-        .update(users)
-        .set({ stripeCustomerId: customer.id })
-        .where(eq(users.id, currentUser.id))
-        .returning();
-    }
-  }
-
-  return Response.json({ currentUser });
+  // const { email = "" } = await request.json();
+  //
+  // if (!email) {
+  //   return Response.json(null, { status: 404 });
+  // }
+  //
+  // const sq = db
+  //   .select({ id: roles.id })
+  //   .from(roles)
+  //   .where(eq(roles.name, "National Sections"))
+  //   .limit(1);
+  //
+  // let currentUser: SelectUser | undefined = await db.query.users.findFirst({
+  //   where(fields, { eq, and }) {
+  //     return and(eq(fields.email, email), eq(fields.roleId, sq));
+  //   },
+  // });
+  //
+  // if (currentUser && !currentUser.stripeCustomerId) {
+  //   const customer = await stripe.customers.create({
+  //     name: `${currentUser.firstname}${
+  //       currentUser.lastname ? ` ${currentUser.lastname}` : ""
+  //     }`,
+  //     email: currentUser.email,
+  //   });
+  //
+  //   if (customer.id) {
+  //     [currentUser] = await db
+  //       .update(users)
+  //       .set({ stripeCustomerId: customer.id })
+  //       .where(eq(users.id, currentUser.id))
+  //       .returning();
+  //   }
+  // }
+  //
+  // return Response.json({ currentUser });
 }
--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -1,12 +1,12 @@
 import Stripe from "stripe";
 import { NextRequest } from "next/server";
 
-const stripe = new Stripe(process.env.STRIPE_API_KEY!);
+// const stripe = new Stripe(process.env.STRIPE_API_KEY!);
 
 export async function GET(_request: NextRequest) {
-  const products = await stripe.prices.list({ active: true });
-
-  const [mainProduct] = products.data;
-
-  return Response.json({ mainProduct });
+  // const products = await stripe.prices.list({ active: true });
+  //
+  // const [mainProduct] = products.data;
+  //
+  // return Response.json({ mainProduct });
 }
