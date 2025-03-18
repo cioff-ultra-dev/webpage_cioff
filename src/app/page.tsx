cbb0dddd5b13b4dc393221d61602d8b3b3df174b
fabiangzvo <faguzman.97@gmail.com>
2025-03-18 14:33:30 -0500

    change styles in navbar items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,10 @@ export default async function Home() {
 
   return (
     <div>
-      <Header text="text-white" className="absolute left-0 right-0 top-0" />
+      <Header
+        text="text-white max-lg:text-black text-roboto text-2xl px-2 py-1 hover:bg-white/40 max-lg:hover:bg-black/20"
+        className="absolute left-0 right-0 top-0"
+      />
       <main>
         <section className="flex flex-col items-center justify-center h-screen bg-cover bg-center relative">
           <Image
