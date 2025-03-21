25b654a97dfc11c3753e0beeb7e3c8a4b02bdc70
Carlos Lopez <celb25@gmail.com>
2025-03-21 09:01:59 -0500

    👌 IMPROVE: Placeholder key for Stripe

--- src/components/common/dashboard.tsx
+++ src/components/common/dashboard.tsx
@@ -243,14 +243,8 @@ export default async function Dashboard({
           </Sheet>
           <DashboardBreadcrumb capitalizeLinks />
           <div className="relative ml-auto flex-1 md:grow-0">
-            {/* <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Search events..."
-              className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
-            /> */}
-
-            <SubscribeButton variant="positive" label={t("subscribe")} />
+            {/* TODO: Uncomment when API STRIPE KEYS are added */}
+            {/* <SubscribeButton variant="positive" label={t("subscribe")} /> */}
           </div>
           <Button variant="outline">
             <Link
