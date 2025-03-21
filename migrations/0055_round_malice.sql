3a4415646e6d704a60d51e10a34b0f966436b79b
Carlos Lopez <celb25@gmail.com>
2025-03-21 17:36:57 -0500

    🐛 FIX: Deprecate scripts sql

--- migrations/0055_round_malice.sql
+++ migrations/0055_round_malice.sql
@@ -1,8 +1,2 @@
-ALTER TABLE "report_festival_activities" DROP CONSTRAINT "report_festival_activities_report_festival_id_report_festival_id_fk";
 --> statement-breakpoint
 ALTER TABLE "rating_questions" ADD COLUMN "active" boolean DEFAULT true NOT NULL;--> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "report_festival_activities" ADD CONSTRAINT "report_festival_activities_report_festival_id_report_festival_id_fk" FOREIGN KEY ("report_festival_id") REFERENCES "public"."report_festival"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
