a20d0ead9535caa02c2a9728404e61abce075127
fabiangzvo <faguzman.97@gmail.com>
2025-03-28 15:36:31 -0500

    change footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ import Facebook from "@/components/common/icons/facebook";
 import News from "@/components/common/news/latest-news";
 import EventList from "@/components/common/event/event-list";
 import { ImpactSection } from "@/components/common/impact/impact-section";
+import Footer from "@/components/common/footer";
 
 export default async function Home() {
   const locale = await getLocale();
@@ -215,21 +216,7 @@ export default async function Home() {
           </div>
         </section>
       </main>
-      <footer className="bg-gray-50 py-4 sm:py-8">
-        <div className="container mx-auto px-4 text-center">
-          <p className="text-gray-800 text-xs sm:text-sm">info@cioff.org</p>
-          <Image
-            src="/logo.png"
-            width="100"
-            height="100"
-            alt="CIOFF Logo"
-            className="inline-block my-6"
-          />
-          <p className="text-gray-800 text-xs sm:text-sm">
-            © CIOFF 1998 - 2024 | cioff.org
-          </p>
-        </div>
-      </footer>
+      <Footer/>
     </div>
   );
 }
--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,20 +1,32 @@
-import Image from "next/image";
+import Link from "next/link";
+import { useTranslations } from "next-intl";
+
+import { Button } from "../ui/button";
 
 function Footer() {
+  const t = useTranslations("footer");
+
   return (
-    <footer className="bg-gray-50 py-4 sm:py-8">
-      <div className="container mx-auto px-4 text-center">
-        <p className="text-gray-800 text-xs sm:text-sm">info@cioff.org</p>
-        <Image
-          src="/logo.png"
-          width="100"
-          height="100"
-          alt="CIOFF Logo"
-          className="inline-block my-6"
-        />
-        <p className="text-gray-800 text-xs sm:text-sm">
-          © CIOFF 1998 - 2024 | cioff.org
-        </p>
+    <footer className="bg-primary py-4 sm:py-8 text-white flex justify-center">
+      <div className="grid grid-cols-2 max-md:grid-cols-1 text-roboto w-2/5 max-md:w-full">
+        <section className="flex flex-col">
+          <p className="text-lg">{t("connect")}</p>
+          <p className="text-md my-4">info@cioff.org</p>
+          <p>
+            CIOFF<span className="text-sm align-top">®</span>
+          </p>
+          <p>{t("place")}</p>
+          <p>{t("address")}</p>
+          <p>{t("additional")}</p>
+          <p>{t("country")}</p>
+        </section>
+        <section className="flex justify-end">
+          <Link href="mailto:legal@cioff.org">
+            <Button className="text-lg hover:text-gray-300">
+              {t("become")}
+            </Button>
+          </Link>
+        </section>
       </div>
     </footer>
   );
